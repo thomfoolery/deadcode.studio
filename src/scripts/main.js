c48125f28653dd9664a62af961c9eceb228b248c
Thomas Yuill <thomas.yuill@gmail.com>
2019-08-08 10:45:48 -0400

    design update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,131 +1,19 @@
-// create illo
-let illo = new Zdog.Illustration({
-  // set canvas with selector
-  element: '#zdog-canvas',
-  dragRotate: true,
-});
+// https://github.com/mattboldt/typed.js/
+import typed from 'typed.js'
 
-// D
-new Zdog.Ellipse({
-  addTo: illo,
-  diameter: 80,
-  quarters: 2,
-  stroke: 20,
-  color: '#C25',
-  translate: { x: -160, },
-});
+// querySelector shortcut
+const $ = document.querySelector.bind(document)
 
-// E
-new Zdog.Shape({
-  addTo: illo,
-  radius: 50,
-  path: [
-    { x: -35, y: -35 },          // start at top left
-    { x:  35, y: -35 },          // line to top right
-    { move: { x: -35, y: 0 } }, // move to bottom left
-    { x:  35, y:  0 },          // line to bottom right
-    { move: { x: -35, y: 35 } }, // move to bottom left
-    { x:  35, y:  35 },          // line to bottom right
-  ],
-  closed: false,
-  stroke: 20,
-  color: '#EA0',
-  translate: {
-    x: -40,
-    y: 0,
-  },
-});
+const DELAY = 0
 
-// A
-new Zdog.Polygon({
-  addTo: illo,
-  radius: 50,
-  sides: 3,
-  stroke: 20,
-  color: '#EA0',
-  translate: {
-    x: 70,
-    y: 15,
-  },
-});
+const typeSpeed = 150
 
-// D
-new Zdog.Ellipse({
-  addTo: illo,
-  diameter: 80,
-  quarters: 2,
-  stroke: 20,
-  color: '#C25',
-  translate: { x: 160, },
-});
-
-// C
-new Zdog.Ellipse({
-  addTo: illo,
-  diameter: 80,
-  quarters: 2,
-  stroke: 20,
-  color: '#C25',
-  translate: {
-    x: -120,
-    y: 120,
-  },
-  rotate: { z: Zdog.TAU/2 }
-});
-
-// O
-new Zdog.Ellipse({
-  addTo: illo,
-  diameter: 80,
-  stroke: 20,
-  color: '#C25',
-  translate: {
-    x: -40,
-    y: 120,
-  },
-});
-
-// D
-new Zdog.Ellipse({
-  addTo: illo,
-  diameter: 80,
-  quarters: 2,
-  stroke: 20,
-  color: '#C25',
-  translate: {
-    x: 50,
-    y: 120,
-  },
-});
-
-// E
-new Zdog.Shape({
-  addTo: illo,
-  radius: 50,
-  path: [
-    { x: -35, y: -35 },          // start at top left
-    { x:  35, y: -35 },          // line to top right
-    { move: { x: -35, y: 0 } }, // move to bottom left
-    { x:  35, y:  0 },          // line to bottom right
-    { move: { x: -35, y: 35 } }, // move to bottom left
-    { x:  35, y:  35 },          // line to bottom right
-  ],
-  closed: false,
-  stroke: 20,
-  color: '#EA0',
-  translate: {
-    x: 180,
-    y: 120,
-  },
-});
-
-
-function animate() {
-  // rotate illo each frame
-  // illo.rotate.y += 0.03;
-  illo.updateRenderGraph();
-  // animate next frame
-  requestAnimationFrame( animate );
+const OPTIONS = {
+  stringsElement: `#intro`,
+  typeSpeed,
+  cursorChar: '_',
+  smartBackspace: true,
 }
-// start animation
-animate();
+
+// typedJS options
+setTimeout(() => new typed(`#canvas`, OPTIONS), DELAY)
