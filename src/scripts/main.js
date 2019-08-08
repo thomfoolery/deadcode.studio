9c9fbeb63dab6d6891ae59a17a5bccc4e9709325
Thomas Yuill <thomas.yuill@gmail.com>
2019-08-08 11:23:16 -0400

    cursor delay and blink update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,13 @@ import typed from 'typed.js'
 const $ = document.querySelector.bind(document)
 
 const DELAY = 0
-
-const typeSpeed = 150
+const typeSpeed = 100
+const startDelay = 2500
 
 const OPTIONS = {
   stringsElement: `#intro`,
   typeSpeed,
+  startDelay,
   cursorChar: '_',
   smartBackspace: true,
 }
